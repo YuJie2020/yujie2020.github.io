1cca041657bc0b33328785b88ec1e23a93b27ed3
YuJie <VegaLau@qq.com>
2021-03-27 10:08:33 +0800

    update cnzz

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,6 @@
    https://github.com/P233/3-Jekyll
 \*---------------------------------*/
 
-// cnzz
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1256860805'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1256860805%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
-
 // Variables
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
