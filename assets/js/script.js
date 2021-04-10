5dda78f3e55af58127fba54433b978fb15afc3a5
YuJie <VegaLau@qq.com>
2021-04-10 11:26:11 +0800

    update style css and script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@
    3-Jekyll Theme
    @author Peiwen Lu (P233)
    https://github.com/P233/3-Jekyll
+   
+   update by Vega Lau
+   https://github.com/YuJie2020
 \*---------------------------------*/
 
 // Variables
@@ -12,10 +15,12 @@ var sidebar    = $('#sidebar'),
     button     = $('#icon-arrow');
     scrollTop  = $('#icon-arrow-up');
     tocbar     = $('#post__toc-trigger');
+	like	   = $('#like');
 
 // Detect window size, if less than 1280px add class 'mobile' to sidebar therefore it will be auto hide when trigger the pjax request in small screen devices.
 if ($(window).width() < 1280) {
   sidebar.addClass('mobile');
+  like.toggleClass("image-center-mobile");
 }
 
 // Tags switcher
